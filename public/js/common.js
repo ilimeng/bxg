0a4966d99d47e6dfbe85af33de985887f1b18c1e
ilimeng <123123@qq.com>
2017-09-20 20:32:35 +0800

    模块化重构

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,30 @@
-
-	NProgress.start();
-
-	NProgress.done();
-
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+define(['jquery','cookie'],function($){	
+	// NProgress.start();
+	// NProgress.done();
+		$('.navs ul').prev('a').on('click', function () {
+			$(this).next().slideToggle();
+		});
+	
+		$("#logOut").click(function () {
+			$.ajax({
+			type : 'post',
+			url : '/api/logout',
+			datatype : 'json',
+			success: function (data) {
+				if(data.code == 200){
+				
+				location.href = 'login'
+				}
+			}
+			})
+		})
+		var flag = $.cookie("PHPSESSID");
+		if(!flag && location.pathname !='/main/login'){
+			location.href = '/main/login'
+		}
+		var loginInfo = $.cookie('loginInfo')
+		loginInfo = loginInfo && JSON.parse(loginInfo)
+		$('.aside .profile img').attr('src',loginInfo.tc_avatar);
+		$('.aside .profile h4').html(loginInfo.tc_name)
+	
+})
