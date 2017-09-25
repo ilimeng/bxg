0a5ee84795353c96ac2e2af845d402ceabf88292
ilimeng <123123@qq.com>
2017-09-25 09:46:24 +0800

    完成课程列表展示

---
+++ public/js/courseList.js
@@ -0,0 +1,14 @@
+define(['jquery','template'],function($,template){
+  $.ajax({
+    type : 'get',
+    url : '/api/course',
+    datatype : 'json',
+    success : function(data){
+     if(data.code == 200){
+       var html = template('courseListTpl',data)
+      $("#courseListInfo").html(html)
+      console.log(data.result)
+     }
+    }
+  })
+})
