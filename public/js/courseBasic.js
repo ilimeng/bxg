562a45528d7a98415a56b527e8d4fc716a4e070d
ilimeng <123123@qq.com>
2017-09-26 12:39:19 +0800

    完成二级分类联动操作

--- public/js/courseBasic.js
+++ public/js/courseBasic.js
@@ -1,4 +1,4 @@
-define(['jquery','util','template'],function($,util,template){
+define(['jquery','util','template','ckeditor'],function($,util,template,CKEDITOR){
 $("#aAdd").addClass('active').parent().parent().show();
 // 获取课程ID
 var csId = util.qs('cs_id')
@@ -10,14 +10,36 @@ $.ajax({
   data : {cs_id : csId},
   dataType : 'json',
   success :function(data){
-    console.log(data)
     if(flag){
       data.result.operate = '课程编辑'
     }else{
       data.result.operate = '课程添加'
     };
     var html = template('besicTpl',data.result);
-    $("#basicInfo").html(html)
+    $("#basicInfo").html(html);
+    $("#change").change(function(){
+      var cgID = $(this).val();
+      $.ajax({
+        type : 'get',
+        url : '/api/category/child',
+        data:{cg_id : cgID},
+        dataType:'json',
+        success : function(data){
+          var tpl = '<option>请选择二级分类...</option>{{each list}}<option value="$value.cg_id">{{$value.cg_name}}</option>{{/each}}';
+          var html = template.render(tpl,{list:data.result})
+          $("#secondType").html(html)
+        }
+      })
+    })
+    CKEDITOR.replace('editor',{
+      toolbarGroups : [
+        { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+        { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+        { name: 'links', groups: [ 'links' ] },
+        { name: 'insert', groups: [ 'insert' ] },
+        { name: 'forms', groups: [ 'forms' ] },
+      ]
+    });
   }
 })
 })
